53cb0f927cd503e8e4dc4b2f3ad63247efbefd27
Ёж Соник <g.mens.hl@gmail.com>
2024-10-25 01:28:41 +0300

    Server client added

---
+++ Filigram/include/ServerClient.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <SFML/Network.hpp>
+#include <nlohmann/json.hpp>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+
+using json = nlohmann::json;
+
+class ServerClient {
+public:
+    explicit ServerClient(const std::string& serverAddress, unsigned short port);
+    bool connect();
+    bool sendRequest(const json& request);
+    bool receiveResponse(json& response);
+    void startReceiverThread();
+
+private:
+    sf::TcpSocket socket;
+    sf::IpAddress serverAddress;
+    unsigned short serverPort;
+    std::queue<json> responseQueue;
+    std::mutex queueMutex;
+    std::condition_variable cv;
+    std::atomic<bool> running;
+    std::thread receiverThread;
+};
