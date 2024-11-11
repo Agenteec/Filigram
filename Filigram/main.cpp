e5c966b0afc5ce02b00d4657580a2883bda7a08c
Ёж Соник <g.mens.hl@gmail.com>
2024-11-11 09:56:35 +0300

    UTF-8 saved

--- Filigram/main.cpp
+++ Filigram/main.cpp
@@ -1,7 +1,10 @@
-#include <MainWindow.h>
+﻿#include <MainWindow.h>
 #include <stdio.h>
+#include <iostream>
 int main()
 {
+	system("chcp 65001");
+
 	std::srand(std::time(nullptr));
 	MainWindow mWindow;
 
